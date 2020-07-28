2984a6da826edfe114940b184cf7507cfd14a36e
sanderdsz <sanderdsz@gmail.com>
2020-07-28 19:17:28 -0300

    structure redone

---
+++ src/components/atoms/Buttons/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Button: React.FC = () => (
+  <div data-testid="Button">
+    <button>Hello world</button>
+  </div>
+);
+
+export default Button;
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import TestComponent from "./TestComponent/TestComponent";
+import Button from "./components/atoms/Buttons"
+
+export { Button, TestComponent };
