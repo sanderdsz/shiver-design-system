6d940d56c9292e457290766e0c6535ff942ae2b7
sanderdsz <sanderdsz@gmail.com>
2020-08-04 13:17:31 -0300

    shiver logo added

---
+++ docs/theme/gatsby-theme-docz/components/Logo/styles.js
@@ -0,0 +1,18 @@
+export const logo = {
+  width: 40,
+}
+
+export const link = {
+  fontWeight: 600,
+  fontSize: 24,
+  color: '#2e3440',
+  textDecoration: 'none',
+  ':hover': {
+    color: '#ebcb8b',
+  },
+}
+
+export const title = {
+  marginTop: 12,
+  marginLeft: 16
+}
