b5ffba77415da80384911e5aba3d9ca3099e5b42
sanderdsz <sanderdsz@gmail.com>
2020-08-03 23:25:24 -0300

    docz cleanup

--- .docz/.cache/async-requires.js
+++
@@ -1,8 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---src-components-atoms-button-button-mdx": () => import("./../../../src/components/atoms/Button/Button.mdx" /* webpackChunkName: "component---src-components-atoms-button-button-mdx" */),
-  "component---src-pages-404-js": () => import("./../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */)
-}
-
