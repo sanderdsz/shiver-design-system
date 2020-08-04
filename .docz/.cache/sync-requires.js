19caaa4f7cd675d1d1762873fbe84a696b5b3e2e
sanderdsz <sanderdsz@gmail.com>
2020-08-03 22:06:26 -0300

    minor theming

---
+++ .docz/.cache/sync-requires.js
@@ -0,0 +1,12 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/home/sander/dev/shiver-design-system/.docz/.cache/dev-404-page.js"))),
+  "component---src-components-atoms-button-button-mdx": hot(preferDefault(require("/home/sander/dev/shiver-design-system/src/components/atoms/Button/Button.mdx"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/home/sander/dev/shiver-design-system/.docz/src/pages/404.js")))
+}
+
--- docs/dracula.js
+++ docs/dracula.js
@@ -1,71 +1,120 @@
-var theme = {
-    plain: {
-      color: "#F8F8F2",
-      backgroundColor: "#434c5e"
+export default {
+  plain: {
+    color: "#F8F8F2",
+    backgroundColor: "#434c5e",
+  },
+  styles: [
+    {
+      types: ["prolog"],
+      style: {
+        color: "#5E81AC",
+      },
     },
-    styles: [
-      {
-        types: ["prolog", "constant", "builtin"],
-        style: {
-          color: "#5E81AC"
-        }
+    {
+      types: ["constant"],
+      style: {
+        color: "#5E81AC",
       },
-      {
-        types: ["inserted", "function"],
-        style: {
-          color: "#88C0D0"
-        }
+    },
+    {
+      types: ["builtin"],
+      style: {
+        color: "#5E81AC",
       },
-      {
-        types: ["deleted"],
-        style: {
-          color: "#BF616A"
-        }
+    },
+    {
+      types: ["inserted"],
+      style: {
+        color: "#88C0D0",
       },
-      {
-        types: ["changed"],
-        style: {
-          color: "#D08770"
-        }
+    },
+    {
+      types: ["function"],
+      style: {
+        color: "#88C0D0",
       },
-      {
-        types: ["punctuation", "symbol"],
-        style: {
-          color: "#D8DEE9"
-        }
+    },
+    {
+      types: ["deleted"],
+      style: {
+        color: "#BF616A",
       },
-      {
-        types: ["string", "char", "tag", "selector"],
-        style: {
-          color: "#88C0D0"
-        }
+    },
+    {
+      types: ["changed"],
+      style: {
+        color: "#D08770",
       },
-      {
-        types: ["keyword", "variable"],
-        style: {
-          color: "#81A1C1",
-          fontStyle: "italic"
-        }
+    },
+    {
+      types: ["punctuation"],
+      style: {
+        color: "#D8DEE9",
       },
-      {
-        types: ["comment"],
-        style: {
-          color: "#c8ccd2"
-        }
+    },
+    {
+      types: ["symbol"],
+      style: {
+        color: "#D8DEE9",
       },
-      {
-        types: ["attr-name"],
-        style: {
-          color: "#8FBCBB"
-        }
+    },
+    {
+      types: ["string"],
+      style: {
+        color: "#88C0D0",
       },
-      {
-        types: ["number"],
-        style: {
-          color: "#B48EAD"
-        }
-      }
-    ]
-  };
-  
-  export default theme;
+    },
+    {
+      types: ["selector"],
+      style: {
+        color: "#88C0D0",
+      },
+    },
+    {
+      types: ["tag"],
+      style: {
+        color: "#88C0D0",
+      },
+    },
+    {
+      types: ["char"],
+      style: {
+        color: "#88C0D0",
+      },
+    },
+    {
+      types: ["variable"],
+      style: {
+        color: "#81A1C1",
+        fontStyle: "italic",
+      },
+    },
+    {
+      types: ["keyword"],
+      style: {
+        color: "#81A1C1",
+        fontStyle: "italic",
+      },
+    },
+    {
+      types: ["comment"],
+      style: {
+        color: "#c8ccd2",
+      },
+    },
+    {
+      types: ["attr-name"],
+      style: {
+        color: "#8FBCBB",
+      },
+    },
+    {
+      types: ["number"],
+      style: {
+        color: "#B48EAD",
+      },
+    },
+  ],
+};
+
+//export default theme;
