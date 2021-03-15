259cd8ce6051f1c79de380f96bbbb5f47589492a
sanderdsz <sanderdsz@gmail.com>
2021-03-15 15:28:31 -0300

    minor styles changes

--- src/components/atoms/Checkbox/Checkbox.tsx
+++ src/components/atoms/Checkbox/Checkbox.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react'
 
-import "./Checkbox.scss";
+import './Checkbox.scss'
 
 export interface CheckboxProps {
   size:
@@ -16,19 +16,6 @@ export const Checkbox: FunctionComponent<CheckboxProps> = ({
   size, 
   ...rest 
 }) => (
-  /*
-  <div className="checkbox">
-    <input type="checkbox" id="checkbox" />
-    <label for="checkbox">{children}</label>
-  </div>
-  */
- /*
-  <label className="container">
-    <input type="checkbox"/>
-    <span className="checkmark"></span>
-    {children}
-  </label>
-  */
   <label className="checkbox">
       <input type="checkbox" />
       {children}
