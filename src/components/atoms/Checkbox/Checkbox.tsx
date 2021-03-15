c60bc5dc25ff3cbb17a2ce77703edf38eb83e92d
sanderdsz <sanderdsz@gmail.com>
2021-03-15 10:45:17 -0300

    default custom checkbox done

--- src/components/atoms/Checkbox/Checkbox.tsx
+++ src/components/atoms/Checkbox/Checkbox.tsx
@@ -16,8 +16,22 @@ export const Checkbox: FunctionComponent<CheckboxProps> = ({
   size, 
   ...rest 
 }) => (
+  /*
   <div className="checkbox">
     <input type="checkbox" id="checkbox" />
     <label for="checkbox">{children}</label>
   </div>
+  */
+ /*
+  <label className="container">
+    <input type="checkbox"/>
+    <span className="checkmark"></span>
+    {children}
+  </label>
+  */
+  <label className="checkbox">
+      <input type="checkbox" />
+      {children}
+      <span className="checkmark"></span>
+  </label>
 )
