2f8db3ba7e8c8d1456900947d0413098d08ddf76
Sander <sanderdsz@gmail.com>
2021-09-11 09:18:51 -0300

    project cleanup

--- .storybook/preview.js
+++
@@ -1,9 +0,0 @@
-import { addParameters } from "@storybook/react";
-
-import theme from "./theme";
-
-addParameters({
-  options: {
-    theme,
-  },
-});
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { Button } from "./sass/components/atoms/Button/Button"
-import { Checkbox } from "./sass/components/atoms/Checkbox/Checkbox"
-export { Button, Checkbox }
